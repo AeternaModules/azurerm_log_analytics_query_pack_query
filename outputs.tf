0566ef9fdf1c97eee30625970883ecb64e335043
Daniel Allen <dan@aeternagroup.net>
2026-02-11 23:53:51 +0000

    Update to schema version v4.59.0

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
-output "log_analytics_query_pack_querys" {
+output "log_analytics_query_pack_queries" {
   description = "All log_analytics_query_pack_query resources"
-  value       = azurerm_log_analytics_query_pack_query.log_analytics_query_pack_querys
+  value       = azurerm_log_analytics_query_pack_query.log_analytics_query_pack_queries
 }
 
